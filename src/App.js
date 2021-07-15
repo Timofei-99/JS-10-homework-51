e49367af032e47cd4738d6908b8972a13b15c926
Shevchenko Timofei <tima9953@gmail.com>
2021-07-15 14:53:31 +0600

    Deletede objects from state array

--- src/App.js
+++ src/App.js
@@ -5,17 +5,14 @@ import Circle from "./Circle/Circle";
 class App extends Component {
     state = {
         numbers: [
-            {number: 5},
-            {number: 11},
-            {number: 16},
-            {number: 23},
-            {number: 32}
+            5,
+            11,
+            16,
+            23,
+            32
         ]
-    }
+    };
 
-    //  getRandomInRange = (min, max) {
-    //     return Math.floor(Math.random() * (max - min + 1)) + min;
-    // }
 
     getRandom = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -24,31 +21,26 @@ class App extends Component {
     changeNumber = () => {
         let numberArray = [];
         while (numberArray.length <= 4) {
-           numberArray.push(this.getRandom(5, 36));
+            const random = this.getRandom(5, 36);
+           if (!numberArray.includes(random)) {
+               numberArray.push(random);
+           }
         };
-      const sortArray = numberArray.sort((a, b) => a > b ? 1 : -1);
-      sortArray.map()
 
-      this.setState({
-          numbers: [
-              {number: sortArray[0]},
-              {number: sortArray[1]},
-              {number: sortArray[2]},
-              {number: sortArray[3]},
-              {number: sortArray[4]}
-          ]
-      })
+        const sortArray = numberArray.sort((a, b) => a > b ? 1 : -1);
+        this.setState({
+            numbers: sortArray
+        });
 
     }
-
     render() {
         return (
             <div>
-                <Circle number={this.state.numbers[0].number}/>
-                <Circle number={this.state.numbers[1].number}/>
-                <Circle number={this.state.numbers[2].number}/>
-                <Circle number={this.state.numbers[3].number}/>
-                <Circle number={this.state.numbers[4].number}/>
+                <Circle number={this.state.numbers[0]}/>
+                <Circle number={this.state.numbers[1]}/>
+                <Circle number={this.state.numbers[2]}/>
+                <Circle number={this.state.numbers[3]}/>
+                <Circle number={this.state.numbers[4]}/>
                 <div className="btn">
                     <button onClick={this.changeNumber}>Click me</button>
                 </div>
